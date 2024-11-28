c804b3651a787bf9feaa2e0c0d14d48ab65485d7
rock-salt-poison <rock.salt.poison@gmail.com>
2024-11-28 11:26:06 +0530

    updates

--- src/Components/Form/EshopForm.jsx
+++ src/Components/Form/EshopForm.jsx
@@ -268,7 +268,7 @@ const EshopForm = () => {
             
             // Navigate to the shop page after a successful submission
             setTimeout(() => {
-              navigate(`../shop?token=${shop_token}`);
+              navigate(`../support/shop/shop-detail?token=${shop_token}`);
             }, 2500);
           }
         } catch (error) {
