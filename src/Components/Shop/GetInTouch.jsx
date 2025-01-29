4346057500688dd2e0e4dd0d78556ea8596b9fcf
rock-salt-poison <rock.salt.poison@gmail.com>
2025-01-29 14:44:42 +0530

    Updates

--- src/Components/Shop/GetInTouch.jsx
+++ src/Components/Shop/GetInTouch.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import map_img from '../../Utils/images/Sell/shop_details/map.svg'
 import contact_img from '../../Utils/images/Sell/shop_details/contact_us.svg'
 import home_img from '../../Utils/images/Sell/shop_details/home.svg'
+import { Link } from 'react-router-dom'
 
 
 function GetInTouch({data}) {
   const details= [
-      {id:1,icon:map_img, text:data.address},
-      {id:2,icon:contact_img, text:data.phone_no_1},
-      {id:3,icon:home_img, text:data.address},
+      {id:1,icon:map_img, text:data.address, redirectTo:`https://www.google.com/maps?q=${data.address}`, target:'_blank'},
+      {id:2,icon:contact_img, text:data.phone_no_1, redirectTo:`tel:${data.phone_no_1}`},
+      {id:3,icon:home_img, text:data.address, redirectTo:`https://www.google.com/maps?q=${data.address}`, target:'_blank'},
   ]
 
   return (
@@ -17,10 +18,10 @@ function GetInTouch({data}) {
         <Box className="contact_row">
           {
               details.map((data)=>{
-                  return <Box className="col" key={data.id}>
+                  return <Link className="col" key={data.id} to={data.redirectTo} target={data.target}>
                           <Box component="img" src={data.icon} className='icon'/>
                           <Typography className="text">{data.text}</Typography>
-                      </Box>
+                      </Link>
               })
           }        
         </Box>
