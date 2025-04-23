56f4c0bfb08da3b40bb3e8bbf99cf6523da26064
rock-salt-poison <rock.salt.poison@gmail.com>
2025-04-23 13:10:12 +0530

    Updates

--- src/Pages/Socialize.jsx
+++ src/Pages/Socialize.jsx
@@ -13,7 +13,37 @@ function Socialize() {
   const token = useSelector((state) => state.auth.userAccessToken);
   // const [userIcon, setUserIcon] = useState(null);
 
+  const items = [
+    {id:1, title:'Updates', linkTo:'', cName:'updates'},
+    {id:2, title:'Feeds', linkTo:'', cName:'feeds'},
+    {id:3, title:'Junction', linkTo:'', cName:'junction'},
+    {id:4, title:'Banners', linkTo:'', cName:'banners'},
+  ]
 
+
+  const handleClick = (e, item) =>{
+    const target = e.target.closest(".updates, .feeds, .junction, .banners, .citizens");
+    if(target){
+        target.classList.toggle('reduceSize3');
+        audio.play();
+        
+        setTimeout(()=>{
+            target.classList.toggle('reduceSize3');
+        },300)
+
+        // setTimeout(()=>{
+        //     if(target.classList.contains('emotional')){
+        //         navigate('../emotional')
+        //     }
+        //     else if(target.classList.contains('unexpected')){
+        //         navigate('../unexpected')
+        //     }
+        //     else if(target.classList.contains('simple')){
+        //         navigate('../simple')
+        //     }
+        // }, 600)
+    }
+}
 
   return (
     <Box className="socialize_wrapper">
@@ -33,40 +63,30 @@ function Socialize() {
         </Box>
         <Box className="col">
             <Box className="col_2">
-                <Box className="item">
-                  <Box component="img" src={paint_stroke} alt="bg" className='item_bg'/>
-                  <Box className="title_container">
-                    <Typography className='title'>Updates</Typography>
-                  </Box>
+              {items.slice(0,2)?.map((item)=>{
+                return <Link key={item.id} className={`${item.cName} item`} onClick={(e)=>handleClick(e, item)}>
+                <Box component="img" src={paint_stroke} alt="bg" className='item_bg'/>
+                <Box className="title_container">
+                  <Typography className='title'>{item.title}</Typography>
                 </Box>
-
-                <Box className="item">
-                  <Box component="img" src={paint_stroke} alt="bg" className='item_bg'/>
-                  <Box className="title_container">
-                    <Typography className='title'>Feeds</Typography>
-                  </Box>
-                </Box>
+              </Link> 
+              })}
             </Box>
             <Box className="col-auto">
-              <Link className="heading_col">
-                <Typography className="heading" variant='h3'>Citizens</Typography> 
+              <Link className="heading_col citizens" onClick={(e)=> handleClick(e)} >
+                <Typography className="heading citizens" variant='h3'>Citizens</Typography> 
               </Link>
             </Box>
 
             <Box className="col_2">
-                <Box className="item">
-                  <Box component="img" src={paint_stroke} alt="bg" className='item_bg'/>
-                  <Box className="title_container">
-                    <Typography className='title'>Junction</Typography>
-                  </Box>
+            {items.slice(2)?.map((item)=>{
+                return <Link key={item.id} className={`${item.cName} item`} to={item.linkTo} onClick={(e)=> handleClick(e, item)}>
+                <Box component="img" src={paint_stroke} alt="bg" className='item_bg'/>
+                <Box className="title_container">
+                  <Typography className='title'>{item.title}</Typography>
                 </Box>
-
-                <Box className="item">
-                  <Box component="img" src={paint_stroke} alt="bg" className='item_bg'/>
-                  <Box className="title_container">
-                    <Typography className='title'>Banners</Typography>
-                  </Box>
-                </Box>
+              </Link> 
+              })}
             </Box>
         </Box>
       </Box>
