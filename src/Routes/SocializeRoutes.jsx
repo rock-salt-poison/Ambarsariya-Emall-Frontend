c392e9d98ac9f497ebbc0c8e72e8253473738e3a
rock-salt-poison <rock.salt.poison@gmail.com>
2025-05-12 17:35:00 +0530

    Updates

--- src/Routes/SocializeRoutes.jsx
+++ src/Routes/SocializeRoutes.jsx
@@ -1,20 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes, Navigate, useParams } from 'react-router-dom';
 import Socialize from '../Pages/Socialize';
 import CityFeeds from '../Pages/Socialize/CityFeeds';
 import Updates from '../Pages/Socialize/Updates';
 import CityLights from '../Pages/Socialize/CityLights';
-
+import { useSelector } from 'react-redux';
+import { getUser } from '../API/fetchExpressAPI';
+import { Box, CircularProgress } from '@mui/material';
+import Login from "../Pages/Serve/Login";
 
 function SocializeRoutes() {
+    const token = useSelector((state) => state.auth.userAccessToken);
+    const [checkUser, setCheckUser] = useState();
+    const [loading, setLoading] = useState(true);
   
+    useEffect(() => {
+      const fetchUserType = async () => {
+        if (token) {
+          try {
+            setLoading(true);
+            const user_type = (await getUser(token))[0].user_type;
+  
+            if (user_type) {
+              setCheckUser(user_type);
+              console.log(user_type);
+            }
+          } catch (e) {
+            console.log(e);
+          } finally {
+            setLoading(false);
+          }
+        }
+      };
+      fetchUserType();
+    }, [token]);
+  
+    const ProtectedRoute = ({ shopElement, memberElement }) => {
+      if (checkUser === "shop") {
+        return shopElement;
+      } else if (checkUser === "member") {
+        return memberElement;
+      } else {
+        return <Navigate to="../login" />;
+      }
+    };
+  
+    if (loading)
+      return (
+        <Box className="loading">
+          <CircularProgress />
+        </Box>
+      );
 
   return (
     <Routes>
-      <Route path="/" element={<Socialize />} />
-      <Route path="/city-feeds" element={<CityFeeds />} />
-      <Route path="/updates" element={<Updates />} />
-      <Route path="/city-lights" element={<CityLights />} />
+      <Route path="/" element={token ? <Socialize /> : <Navigate to="login" />
+      } />
+      <Route path="login" element={<Login />} />
+      <Route path="/city-feeds" element={
+        <ProtectedRoute
+            shopElement={<CityFeeds />}
+            memberElement={<CityFeeds />}
+          />
+        } />
+      <Route path="/updates" element={
+        <ProtectedRoute
+            shopElement={<Updates />}
+            memberElement={<Updates />}
+          />
+        } />
+      <Route path="/city-lights" element={
+        <ProtectedRoute
+            shopElement={<CityLights />}
+            memberElement={<CityLights />}
+          />
+        } />
       
     </Routes>
   );
